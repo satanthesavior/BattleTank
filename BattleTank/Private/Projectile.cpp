461c52427389c8f66d0ce5ef3aef2c1c82ce7c37
satanthesavior <38424112+satanthesavior@users.noreply.github.com>
2018-11-01 18:54:53 +0100

    Add files via upload
    BattleTank Visual Studio files.

---
+++ BattleTank/Private/Projectile.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "BattleTank.h"
+#include "ParticleEmitterInstances.h"
+#include "ParticleDefinitions.h"
+#include "Classes/PhysicsEngine/RadialForceComponent.h"
+#include "Classes/PhysicsEngine/RadialForceComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Classes/Components/SceneComponent.h"
+#include "Classes/Kismet/GameplayStatics.h"
+#include "Classes/Engine/World.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
+	SetRootComponent(CollisionMesh);
+	CollisionMesh->SetNotifyRigidBodyCollision(true);
+	CollisionMesh->SetVisibility(false);
+	
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast"));
+	LaunchBlast->SetupAttachment(CollisionMesh);
+	
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
+	
+	ImpactBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Impact Blast"));
+	ImpactBlast->SetupAttachment(CollisionMesh);
+	ImpactBlast->bAutoActivate = false;
+
+	ExplosionForce = CreateDefaultSubobject<URadialForceComponent>(FName("Explosion Force"));
+	ExplosionForce->SetupAttachment(CollisionMesh);
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CollisionMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+
+}
+
+void AProjectile::LaunchProjectile(float Speed)
+{
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
+	ProjectileMovement->Activate();
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	LaunchBlast->Deactivate();
+	ImpactBlast->Activate();
+	ExplosionForce->FireImpulse();
+
+	SetRootComponent(ImpactBlast);
+	CollisionMesh->DestroyComponent();
+
+	UGameplayStatics::ApplyRadialDamage(
+		this,
+		ProjectileDamage,
+		GetActorLocation(),
+		ExplosionForce->Radius,
+		UDamageType::StaticClass(),
+		TArray<AActor*>() //Damages all Acotrs
+		);
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
+	
+}
+
+void AProjectile::OnTimerExpire()
+{
+	Destroy();
+}
---
+++ BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" //Always last
+
+class UTankTankAimingComponent;
+
+/**
+* Responsible for helping the player aim.
+*/
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+		void FoundAimingComponent(UTankAimingComponent* AimCompRef);
+
+	UFUNCTION()
+		void OnPossedTankDeath();
+
+private:
+	void SetPawn(APawn * InPawn);
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditDefaultsOnly)
+		float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditDefaultsOnly)
+		float CrosshairYLocation = 0.3333;
+
+	UPROPERTY(EditDefaultsOnly)
+		float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+};
